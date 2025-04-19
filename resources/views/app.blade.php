e937f12b6498c91e56e7d224b16e7c31780e5ca2
jaredkc <jaredkc@gmail.com>
2025-04-19 13:55:30 +0800

    Update app name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,14 +23,16 @@
         <style>
             html {
                 background-color: oklch(1 0 0);
+                color: oklch(0.145 0 0);
             }
 
             html.dark {
                 background-color: oklch(0.145 0 0);
+                color: oklch(1 0 0);
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Jared Cornwall') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
